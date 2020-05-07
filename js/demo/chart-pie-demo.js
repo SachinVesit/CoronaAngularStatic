25b705560862541a7991d9e6456cbf549788758f
Sachin <sachinvesit@gmail.com>
2020-05-07 17:57:17 +0530

    Second Day Changes

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -10,7 +10,7 @@ var myPieChart = new Chart(ctx, {
     labels: ["Direct", "Referral", "Social"],
     datasets: [{
       data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      backgroundColor: ['#ff9a00', '#ff0000', '#056707'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
